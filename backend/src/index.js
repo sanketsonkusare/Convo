f3eb4bb980dd8885ea270b6f9efaa70be600883a
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-05-28 23:24:44 +0530

    feat: Update frontend dependencies and implement authentication store
    - Added new dependencies: @tailwindcss/vite, axios, lucide-react, react-hot-toast, react-router-dom, tailwindcss, zustand, and daisyui.
    - Removed unused assets: vite.svg and react.svg.
    - Deleted App.css and migrated styles to Tailwind CSS.
    - Refactored App.jsx to include routing and authentication checks.
    - Created AuthStore for managing authentication state and socket connections.
    - Implemented axios instance for API calls.
    - Created placeholder components for Navbar and various pages (HomePage, SignupPage, LoginPage, SettingsPage, ProfilePage).
    - Updated Vite configuration to include Tailwind CSS plugin.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,17 @@ import messageroutes from "./routes/messageroute.js";
 import {connectDB} from "./lib/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 
 
 app.use("/api/auth", authroutes);
