45eef8ddd9c01f080047fe89cc06b08628ccbd67
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-07-05 23:38:19 +0530

    fix:

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,16 +6,19 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
+
 app.use(express.json({limit:"8mb"}));
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: process.env.NODE_ENV === "production" 
+        ? [process.env.CLIENT_URL, "https://*.onrender.com"]
+        : "http://localhost:5173",
+    credentials: true
 }));
-
 
 app.use("/api/auth", authroutes);
 app.use("/api/message", messageroutes);
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try{
-        const connect = await mongoose.connect(process.env.MONGO_URL);
+        const connect = await mongoose.connect(process.env.MONGO_URI);
         console.log(`Connected to db:${connect.connection.host}`)
     } catch (error) {
         console.log(`Some error occured:`,error);
